f5f2c2cdb3c7f5ab455d40c19f1fb7abf8e464e6
34elo <evsenik23@mail.ru>
2025-02-03 22:52:33 +0300

    fix README.md

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,14 @@
-print('hello world')
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def empty():
+    return jsonify({'message': 'Everything is fine)))'})
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=False, host='0.0.0.0')
+
+# Просто, чтоб не было Hello world))
